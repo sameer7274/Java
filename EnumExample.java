aa314749c206fd55b5546b0bee4179f76fc6f6a1
sameer <sameerazzj2@gmail.com>
2025-01-20 17:08:53 +0530

    Enumcommit

---
+++ EnumExample.java
@@ -0,0 +1,15 @@
+enum DayOfWeek{
+    SUNDAY,MONDAY,TUESDAY,THURSADY,FRIDAY,SATURDAY
+}
+public class EnumExample {
+    public static void main(String[] args) {
+        DayOfWeek today=DayOfWeek.MONDAY;
+        System.out.println("Today is "+today);
+        System.out.println("Days of the week");
+        for(DayOfWeek day:DayOfWeek.values()){
+            System.out.println(day);
+        }
+        
+    }
+    
+}
